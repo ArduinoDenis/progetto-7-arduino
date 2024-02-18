ac02f2c2c7310af9948b8a6928d751bda3e39622
arduinodenis <90153974+ArduinoDenis@users.noreply.github.com>
2024-02-18 19:38:55 +0100

    Update sensore-di-parcheggio.ino

--- sensore-di-parcheggio.ino
+++ sensore-di-parcheggio.ino
@@ -1,77 +1,63 @@
-//Sensore di parcheggio
+// Parking Sensor
 
+// Define sensor pins
 int triggerPort = 7;
-
 int echoPort = 8;
+int buzzer = 9;
 
-int cicalino = 9;
-
+// Define time variables
 unsigned long time;
-
-unsigned long lampeggio_time;
-
-unsigned long pausa_time;
+unsigned long blink_time;
+unsigned long pause_time;
 
 void setup() {
-
-pinMode( triggerPort, OUTPUT );
-
-pinMode( echoPort, INPUT );
-
-pinMode( cicalino, OUTPUT );
-
-Serial.begin( 9600 );
-
-Serial.println( "Sensore ultrasuoni: ");
-
+  // Set pin modes
+  pinMode(triggerPort, OUTPUT);
+  pinMode(echoPort, INPUT);
+  pinMode(buzzer, OUTPUT);
+  
+  // Initialize serial communication
+  Serial.begin(9600);
+  Serial.println("Ultrasonic Sensor:");
 }
 
 void loop() {
-
-//porta bassa l'uscita del trigger
-
-digitalWrite( triggerPort, LOW );
-
-//invia un impulso di 10microsec su trigger
-
-digitalWrite( triggerPort, HIGH );
-
-delayMicroseconds( 10 );
-
-digitalWrite( triggerPort, LOW );
-
-long duration = pulseIn( echoPort, HIGH );
-
-long r = 0.034 * duration / 2;
-
-Serial.print( "durata: " );
-
-Serial.print( duration );
-
-Serial.print( " , " );
-
-Serial.print( "distanza: " );
-
-if( duration > 38000 ) Serial.println( "fuori portata");
-
-else { Serial.print( r ); Serial.println( "cm" );}
-
-if( r > 3 && r <= 200){
-
-delay(r*10);
-
-digitalWrite(cicalino, HIGH);
-
-delay(r*10); }
-
-if( r <= 3){
-
-digitalWrite(cicalino, HIGH);
-
-delay(1000);}
-
-digitalWrite(cicalino, LOW);
-
-delay(10);
-
+  // Lower trigger pin output
+  digitalWrite(triggerPort, LOW);
+  
+  // Send a 10 microsecond pulse on trigger pin
+  digitalWrite(triggerPort, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPort, LOW);
+  
+  // Measure the duration of the echo pulse
+  long duration = pulseIn(echoPort, HIGH);
+  
+  // Calculate distance based on duration
+  long distance = 0.034 * duration / 2;
+  
+  // Print the results to serial monitor
+  Serial.print("Duration: ");
+  Serial.print(duration);
+  Serial.print(", Distance: ");
+  
+  if (duration > 38000) {
+    Serial.println("Out of range");
+  } else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  
+  // Activate buzzer based on distance
+  if (distance > 3 && distance <= 200) {
+    delay(distance * 10);
+    digitalWrite(buzzer, HIGH);
+    delay(distance * 10);
+  } else if (distance <= 3) {
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
+  }
+  
+  digitalWrite(buzzer, LOW);
+  delay(10);
 }
